72c64138f8480a690b511583a363c59f7f653565
updateNobu <takenobu0731nichidai@gmail.com>
2020-10-26 03:36:29 +0000

    add model detail

--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,2 +1,13 @@
 class Roadmap < ApplicationRecord
+  has_many :roadmapShows, dependent: :destroy
+  
+  validates :title, {presence: true, length: {maximum: 140}}
+  validates :stady_time_week, {presence: true}
+  validates :stady_time_holiday, {presence: true}
+  validates :period_stady, {presence: true}
+  validates :total_stady_time, {presence: true}
+  validates :total_comment, {presence: true, length: {maximum: 140}}
+  validates :category_id, {presence: true}
+  
+  
 end
--- app/models/roadmapshow.rb
+++ app/models/roadmapshow.rb
@@ -1,2 +1,8 @@
 class Roadmapshow < ApplicationRecord
+  belongs_to :roadmaps, optional: true
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :method, {presence: true, length: {maximum: 140}}
+  validates :time_required, {presence: true}
+  validates :comment, {presence: true, length: {maximum: 140}}
+  validates :roadmap_id, {presence: true}
 end
