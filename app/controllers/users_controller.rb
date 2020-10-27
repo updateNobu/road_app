e6af3011c5f81ef9b4e5861a82882e76db6351f7
updateNobu <takenobu0731nichidai@gmail.com>
2020-10-27 03:36:44 +0000

    add route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/user/show/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 
   def showpage
-    @user = User.find_by(id: 1)
+    @user = User.find_by(id: params[:id])
   end
   
   def roadeach
-    @user = User.find_by(id: 1)
+    @user = User.find_by(id: params[:id])
     @mymaps = Roadmapshow.where(roadmap_id: @user.mymap)
   end
   
-  def singup
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,9 @@ Rails.application.routes.draw do
   get "roadmap/mapedit/:id" => "roadmaps#mapedit"
   
   get "user/new" => "users#new"
-  get "user/show" => "users#showpage"
-  get "user/road" => "users#roadeach"
+  get "user/show/:id" => "users#showpage"
+  get "user/road/:id" => "users#roadeach"
+  post "user/create" => "users#create"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
