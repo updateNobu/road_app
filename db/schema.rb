603af783fcc8cb08d337d2a9fc503fb113d04494
updateNobu <takenobu0731nichidai@gmail.com>
2020-10-29 23:22:11 +0000

    Make a image_name ob db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_075238) do
+ActiveRecord::Schema.define(version: 2020_10_29_231450) do
 
   create_table "roadmaps", force: :cascade do |t|
     t.text "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_075238) do
     t.text "comment"
     t.string "link"
     t.integer "mymap"
+    t.string "image_name"
   end
 
 end
